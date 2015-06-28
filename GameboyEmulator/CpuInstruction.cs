ab1685fc67633d65db86e3e38767a550b965923a
Thomas Holmes <thomasholmes@outlook.com>
2015-06-28 20:42:49 +0100

    Added Gameboy and CpuInstruction classes.

---
+++ GameboyEmulator/CpuInstruction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameboyEmulator
+{
+    public class CpuInstruction
+    {
+        public int OpCode { get; private set; }
+        public int Cycles { get; private set; }
+        public MethodInfo MethodInfo { get; private set; }
+        public Cpu Cpu { get; private set; }
+        public string AssemblyInstruction { get; private set; }
+
+        public CpuInstruction() { }
+
+        public int Execute()
+        {
+            Debug.WriteLine("0x{0:X4}\t{1}", Cpu.PC, AssemblyInstruction);
+            MethodInfo.Invoke(Cpu, null);
+            Cpu.Timer += Cycles;
+            return Cycles;
+        }
+
+        public static CpuInstruction GetInstruction(Cpu cpu, MethodInfo methodInfo)
+        {
+            var opAttr = methodInfo.GetCustomAttributes()
+                .FirstOrDefault(a => a.GetType() == typeof(OpAttribute) || a.GetType().GetTypeInfo().IsSubclassOf(typeof(OpAttribute))) as OpAttribute;
+
+            var cpuIns = new CpuInstruction()
+            {
+                MethodInfo = methodInfo,
+                Cycles = opAttr.Cycles,
+                OpCode = opAttr.OpCode,
+                Cpu = cpu,
+                AssemblyInstruction = opAttr.AssemblyInstruction
+            };
+
+            return cpuIns;
+        }
+    }
+}
---
+++ GameboyEmulator/Gameboy.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameboyEmulator
+{
+    public class Gameboy
+    {
+        public delegate void ProcessEventHandler(Gameboy sender, EventArgs e);
+        public event ProcessEventHandler ProcessEvent;
+
+        public delegate void DrawEventHandler(Gameboy sender, Pixel[][] pixels, EventArgs e);
+        public event DrawEventHandler DrawEvent;
+
+        public Cpu Cpu { get; private set; }
+        public Mmu Mmu { get; private set; }
+        public Gpu Gpu { get; private set; }
+
+        public Gameboy()
+        {
+            Cpu = new Cpu();
+            Mmu = new Mmu();
+            Cpu.Mmu = Mmu;
+            Gpu = new Gpu(Cpu);
+            Mmu.Gpu = Gpu;
+
+            Gpu.DrawEvent += Gpu_DrawEvent;
+        }
+
+        private void Gpu_DrawEvent(Gpu sender, Pixel[] pixels, EventArgs e)
+        {
+            var screen = new Pixel[160][];
+            screen[0] = new Pixel[144];
+            int w = 0;
+            int h = 0;
+            foreach(var p in pixels)
+            {
+                screen[w][h] = p;
+                w++;
+                if(w == 160)
+                {
+                    w = 0;
+                    h++;
+                    screen[h] = new Pixel[144];
+                }
+            }
+            if(DrawEvent != null)
+            {
+                DrawEvent.Invoke(this, screen, new EventArgs());
+            }
+        }
+        
+        public void Begin()
+        {
+            while(true)
+            {
+                int cycles = Process();
+
+                // much too slow
+                Task.Delay(cycles * 10);
+            }
+            
+        }
+
+        public int Process()
+        {
+            if (ProcessEvent != null)
+            {
+                ProcessEvent.Invoke(this, new EventArgs());
+            }
+
+            int cycles = Cpu.Process();
+            Gpu.Step();
+            return cycles;
+        }
+
+    }
+}
